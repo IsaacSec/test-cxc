25705a09310b7ce878ff88f6bb8fc3b6b9e2dbb1
Isaac Secundino <isaacsecundino3@gmail.com>
2024-07-20 14:02:00 -0600

    Initial commit

---
+++ src/main/java/com/isaac/testcxc/TestCxcApplication.java
@@ -0,0 +1,13 @@
+package com.isaac.testcxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCxcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCxcApplication.class, args);
+    }
+
+}
